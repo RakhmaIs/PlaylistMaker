396afb44a47b6dd208ac5eb96dc52db212bd7a6c
Iskhakov Shamil <forinfoiskhakov@gmail.com>
2024-03-23 11:54:52 +0300

    Home tasks - 8 sprint - fix: "dark theme" text visible delete: edgeToEdge() from all Activities

--- app/src/main/java/com/android/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/android/playlistmaker/SettingsActivity.kt
@@ -3,13 +3,11 @@ package com.android.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
         val backArrowButton = findViewById<Button>(R.id.back_arrow)
         backArrowButton.setOnClickListener {
